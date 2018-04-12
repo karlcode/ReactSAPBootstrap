454d509b0dc4e1ff2bce37111ac4861f71f72af5
Karl Li <karl.l1@hotmail.com>
2018-04-12 03:14:22 +0000

    Revision on bootstrapping code

--- js/app.js
+++ js/app.js
@@ -1,38 +1,175 @@
-var CurrencyDisplay = React.createClass({
-	render: function() {
-		var computedPrice = new Number( this.props.value );
-		computedPrice = Math.round( computedPrice * 100 )/100;
-		computedPrice = computedPrice.toFixed(2).toLocaleString();
+var FormGroup = ReactBootstrap.FormGroup;
+var Label = ReactBootstrap.ControlLabel;
+var Input = ReactBootstrap.FormControl;
+var Static = ReactBootstrap.FormControl.Static;
+var Button = ReactBootstrap.Button;
 
-		return(
-			<span>
-				{computedPrice}
-				<small className="text-muted"> EUR</small>
-			</span>
-		)
-	}
+var FilterEntryBox = React.createClass({
+getInitialState: function() {
+return {
+filterText: "",
+}
+},
+onTextChange: function(event) {
+this.setState({filterText: event.target.value});
+this.props.callback( event.target.value );
+},
+render: function() {
+return(
+<Input type="text"
+	value={this.state.filterText}
+	placeholder="Filter Product Name"
+	onChange={this.onTextChange}/>
+)
+}
+});
+
+var CategoryDisplay = React.createClass({
+getInitialState: function() {
+return {
+categoryCache: [],
+loaded: false,
+};
+},
+componentDidMount: function() {
+var odataUrl = "/Northwind/V3/Northwind/Northwind.svc/";
+
+$.ajax({
+url: odataUrl + "Categories" ,
+dataType: 'json',
+cache: false
+})
+.done( function( data, textStatus, jqXHR ) {
+	this.setState( {
+		categoryCache: data.value,
+		loaded: true,
+	} );
+}.bind(this)
+)
+.fail( function( jqXHR, textStatus, errorThrown ) {
+console.log("Error", jqXHR, textStatus, errorThrown );
+alert( "An arror occurred while retrieving the category list from the server: " + textStatus );
+});
+},
+findRow: function( id ) {
+var result = $.grep(this.state.categoryCache, function(e){ return e.CategoryID == id; });
+if ( result.length > 0 ) return result[0].CategoryName;
+else return null;
+},
+render: function() {
+return(
+<span>
+	{ this.state.loaded ? this.findRow( this.props.id ) : "...loading..." }
+</span>
+)
+}
 })
 
+var AvailableDisplay = React.createClass({
+render: function() {
+return(
+<span className={this.props.value ? "discontinued" : "available"}>
+	{this.props.value ? "Discontinued" : "Available"}
+</span>
+)
+}
+});
+
+var CurrencyDisplay = React.createClass({
+render: function() {
+var computedPrice = new Number( this.props.value );
+computedPrice = Math.round( computedPrice * 100 )/100;
+computedPrice = computedPrice.toFixed(2).toLocaleString();
+
+return(
+<span>
+	{computedPrice}
+	<small className="text-muted"> EUR</small>
+</span>
+)
+}
+});
+
 var ModalProductDetail = React.createClass({
+getInitialState: function() {
+return {
+OrderCount: "",
+orderSubmitted: false,
+}
+},
+onOrderChange: function(event) {
+this.setState({OrderCount: event.target.value});
+},
+onSaveClick: function() {
+this.setState({
+orderSubmitted: true,
+});
+
+setTimeout( this.closeAndReset, 2000 );
+},
+closeAndReset: function() {
+$('#product-detail').modal('hide');
+this.setState({
+orderSubmitted: false,
+OrderCount: "",
+});
+},
 render: function() {
 return (
 <div className="modal fade" tabIndex="-1" role="dialog" id="product-detail">
 	<div className="modal-dialog modal-lg" role="document">
 		<div className="modal-content">
 			<div className="modal-header">
-				<h4 className="modal-title">Title</h4>
+				<h4 className="modal-title">Product Details - {this.props.row.ProductName}</h4>
 			</div>
 			<div className="modal-body">
-				<p>Product Name: {this.props.row.ProductName}</p>
-				<p>Unit Price: <CurrencyDisplay value={this.props.row.UnitPrice} /></p>
-				<p>Status: {this.props.row.Discontinued}</p>
-				<p>Category: {this.props.row.CategoryID} </p>
+				{ this.state.orderSubmitted ? <div className="alert alert-success" role="alert">
+					<strong>Success!</strong>&nbsp;
+					{this.state.OrderCount} units of {this.props.row.ProductName} have been ordered.
+				</div> : null }
+				<form className="form-horizontal">
+					<FormGroup>
+						<Label className="col-sm-3">Product Name</Label>
+						<Static className="col-sm-9">{this.props.row.ProductName}</Static>
+					</FormGroup>
+					<FormGroup>
+						<Label className="col-sm-3">Unit Price</Label>
+						<Static className="col-sm-9">
+							<CurrencyDisplay value={this.props.row.UnitPrice} />
+						</Static>
+					</FormGroup>
+					<FormGroup>
+						<Label className="col-sm-3">Status</Label>
+						<Static className="col-sm-9">
+							<AvailableDisplay value={this.props.row.Discontinued} />
+						</Static>
+					</FormGroup>
+					<FormGroup>
+						<Label className="col-sm-3">Category</Label>
+						<Static className="col-sm-9">
+							<CategoryDisplay id={this.props.row.CategoryID} />
+						</Static>
+					</FormGroup>
+					<FormGroup>
+						<Label className="col-sm-3">Units In Stock</Label>
+						<Static className="col-sm-9">{this.props.row.UnitsInStock}</Static>
+					</FormGroup>
+					<FormGroup>
+						<Label className="col-sm-3">Order Amount</Label>
+						<div className="col-sm-6">
+							<Input type="number"
+									value={this.state.OrderCount}
+									placeholder="Number of units to order"
+									onChange={this.onOrderChange}
+									disabled={this.state.orderSubmitted}/>
+						</div>
+					</FormGroup>
+				</form>
 			</div>
-				
-
-
 			<div className="modal-footer">
-				<button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+				<Button bsStyle="primary" onClick={this.onSaveClick}
+					disabled={this.state.OrderCount == "" || this.state.orderSubmitted}>Save</Button>
+				<Button onClick={this.closeAndReset}>Close</Button>
 			</div>
 		</div>
 	</div>
@@ -50,9 +187,14 @@ return (
 			<div className="navbar-brand">Product Overview</div>
 		</div>
 
-		<div className="nav navbar-nav navbar-right">
-			<p className="navbar-text">Items: {this.props.count}</p>
-		</div>
+		<ul className="nav navbar-nav navbar-right">
+  				<li className="navbar-form form-group">
+  					<FilterEntryBox callback={this.props.filterCallback} />
+  				</li>
+  				<li>
+  					<p className="navbar-text">Items: {this.props.count}</p>
+  				</li>
+  			</ul>
 	</div>
 </nav>
 )
@@ -69,13 +211,11 @@ render: function() {
 			<h4>{this.props.row.ProductName}</h4>
 			<p> {this.props.row.QuantityPerUnit}</p>
 		</div>
-		<div className="col-sm-3 text-right ">
+		<div className="col-sm-3 text-right top">
 			<h4>
 				<CurrencyDisplay value={this.props.row.UnitPrice} />
-				
-
 			</h4>
-			<p>{this.props.row.Discontinued ? "Discontinued" : "Available"}</p>
+			<AvailableDisplay value={this.props.row.Discontinued} />
 		</div>
 		<div className="col-sm-1 center">
 			<span className="glyphicon glyphicon-chevron-right pull-right" aria-hidden="true"></span>
@@ -90,10 +230,10 @@ var ProductList = React.createClass({
 getInitialState: function() {
 return {
 products: [],
+origProducts: [],
 selectedRow: null,
 };
 },
-
 onListBoxClick: function( row ) {
 this.setState({selectedRow: row});
 
@@ -101,7 +241,6 @@ setTimeout( function() {
 $('#product-detail').modal("show");
 });
 },
-
 componentDidMount: function() {
 var odataUrl = "/Northwind/V3/Northwind/Northwind.svc/";
 
@@ -111,7 +250,10 @@ dataType: 'json',
 cache: false
 })
 .done( function( data, textStatus, jqXHR ) {
-	this.setState( {products: data.value } )
+	this.setState( {
+		products: data.value,
+		origProducts: data.value,
+	} )
 }.bind(this)
 )
 .fail( function( jqXHR, textStatus, errorThrown ) {
@@ -119,7 +261,17 @@ console.log("Error", jqXHR, textStatus, errorThrown );
 alert( "An arror occurred while retrieving data from the server: " + textStatus );
 });
 },
-
+titleBarFilter: function( text ) {
+if ( text.trim().length > 0 ) {
+var newList = this.state.origProducts.filter( function(row){
+	return row.ProductName && row.ProductName.toLowerCase().indexOf( text ) >= 0;
+});
+this.setState({ products : newList });
+}
+else {
+this.setState({ products: this.state.origProducts });
+}
+},
 render: function() {
 var productListBoxes = this.state.products.map( function(row) {
 return(
@@ -130,7 +282,7 @@ return(
 
 return (
 <div>
-	<TitleBar count={this.state.products.length}/>
+	<TitleBar count={this.state.products.length} filterCallback={this.titleBarFilter}/>
 	<div className="list-group">
 		{productListBoxes}
 	</div>
